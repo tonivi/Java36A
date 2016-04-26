5c18ee848cdb746f5bb4e13d2d4ba933d8c60b6d
tonivi <tonivitruong@yahoo.com>
2016-04-26 14:29:28 -0700

    Update and rename CIS36AFall2015ToniviTruongWorkAtHome.java to Cis36AFall2015ToniviTruongWorkAtHome.java

--- Cis36AFall2015ToniviTruongWorkAtHome.java
+++ Cis36AFall2015ToniviTruongWorkAtHome.java
@@ -1,5 +1,3 @@
-package cis36afall2015tonivitruongworkathome;
-
 import java.util.Scanner;
 
 public class CIS36AFall2015ToniviTruongWorkAtHome {
@@ -61,43 +59,3 @@ public class CIS36AFall2015ToniviTruongWorkAtHome {
 
     }
 }
-
-/** Output
-
-Enter array size: 10
-
-Enter an int: 0
-Enter an int: 1
-Enter an int: 2
-Enter an int: 3
-Enter an int: 4
-Enter an int: 5
-Enter an int: 6
-Enter an int: 7
-Enter an int: 8
-Enter an int: 9
-
-Original  Array
- Index 0 : 0
- Index 1 : 1
- Index 2 : 2
- Index 3 : 3
- Index 4 : 4
- Index 5 : 5
- Index 6 : 6
- Index 7 : 7
- Index 8 : 8
- Index 9 : 9
-
-Sorted Array
- Index 0 : 1
- Index 1 : 3
- Index 2 : 5
- Index 3 : 7
- Index 4 : 9
- Index 5 : 8
- Index 6 : 6
- Index 7 : 4
- Index 8 : 2
- Index 9 : 0
- **/
